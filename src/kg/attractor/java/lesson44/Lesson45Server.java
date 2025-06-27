34668c08610e0207b7bdb9c97a6d730bdf367615
Kaseiinov_I <izlax6002@gmail.com>
2025-06-27 15:27:52 +0600

    Added ftlh for porfile and made user profile

--- src/kg/attractor/java/lesson44/Lesson45Server.java
+++ src/kg/attractor/java/lesson44/Lesson45Server.java
@@ -3,15 +3,18 @@ package kg.attractor.java.lesson44;
 import com.sun.net.httpserver.HttpExchange;
 import kg.attractor.java.server.ContentType;
 import kg.attractor.java.server.ResponseCodes;
+import kg.attractor.java.server.models.Book;
 import kg.attractor.java.server.models.User;
+import kg.attractor.java.utils.JsonUtils;
 import kg.attractor.java.utils.Utils;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.Map;
+import java.util.*;
 
 public class Lesson45Server extends Lesson44Server {
+    private Optional<User> loggedUser = Optional.of(new User());
 
     public Lesson45Server(String host, int port) throws IOException {
         super(host, port);
@@ -19,6 +22,17 @@ public class Lesson45Server extends Lesson44Server {
         registerPost("/login", this::loginPost);
         registerGet("/register", this::registerGet);
         registerPost("/register", this::registerPost);
+        registerGet("/profile", this::profileGetFreemarkerHandler);
+    }
+
+    private void profileGetFreemarkerHandler(HttpExchange exchange){
+        renderTemplate(exchange, "profile.ftlh", getUserProfileDataModel(loggedUser.orElse(null)));
+    }
+
+    private Map<String, User> getUserProfileDataModel(User user) {
+        Map<String, User> mapUser = new HashMap<>();
+        mapUser.put("user", user);
+        return mapUser;
     }
 
     private void registerPost(HttpExchange exchange) {
@@ -55,24 +69,22 @@ public class Lesson45Server extends Lesson44Server {
     }
 
     private void loginPost(HttpExchange exchange) {
-//        String cType = getContentType(exchange);
-//        String raw = getBody(exchange);
-//        Map<String, String> parsed = Utils.parseUrlEncoded(raw, "&");
-//
-//        String fmt = "<p>Необработанные данные: <b>%s</b></p>"
-//                + "<p>Content-type: <b>%s</b></p>"
-//                + "<p>После обработки: <b>%s</b></p>";
-//
-//        String data = String.format(fmt, raw, cType, parsed);
-//
-//        try {
-//            sendByteData(exchange, ResponseCodes.OK,
-//                    ContentType.TEXT_HTML, data.getBytes());
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        String cType = getContentType(exchange);
+        String raw = getBody(exchange);
+        Map<String, String> parsed = Utils.parseUrlEncoded(raw, "&");
 
-        redirect(exchange, "/");
+
+        if(User.getUsers().stream().anyMatch(e -> e.getEmail().equals(parsed.get("email")))) {
+            Optional<User> user = User.getUsers().stream().filter(e -> e.getEmail().equals(parsed.get("email"))).findFirst();
+            if(user.get().getPassword().equals(parsed.get("user-password"))){
+                loggedUser = user;
+                redirect(exchange, "/profile");
+            }else {
+                redirect(exchange, "/login");
+            }
+        }else{
+            redirect(exchange, "/login");
+        }
 
     }
 
